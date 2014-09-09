1bb3798206cfa59ac5bbfa3982e14ab9692601da
Sean Roberts <sean.roberts90@gmail.com>
2014-09-08 23:35:43 -0400

    Refactoring and adding QueryData object type to make using results easier

--- src/AppStoreQuery.js
+++ src/AppStoreQuery.js
@@ -8,6 +8,29 @@
 var request = require('request');
 
 
+// Our formal response cnstr function
+// to make gathering information about the
+// apps easier
+var QueryData = function AppData( url, apps ){
+
+  this.source = 'This data is happily provided by Apple :)';
+  this.url = url;
+  this.data = apps || [];
+  this.each = function each(eachFunc){
+    this.data.forEach(function(app, index){
+      eachFunc(app, index);
+    });
+  };
+  this.first = function(){
+    return this.data.length > 0 ? this.data[0] : undefined;
+  }
+  this.last = function(){
+    return this.data.length > 0 ? this.data[this.data.length - 1] : undefined;
+  };
+};
+
+
+
 // Base Url for our requests
 // Note: only http seems to be supported
 var ITUNES_URL = 'http://itunes.apple.com/',
@@ -15,6 +38,8 @@ var ITUNES_URL = 'http://itunes.apple.com/',
 
   // Handle sending the request to iTunes
   get = function( url, cbSuccess, cbFail){
+
+    var start = new Date();
 
     request(url, function (error, response, body) {
 
@@ -25,7 +50,7 @@ var ITUNES_URL = 'http://itunes.apple.com/',
       }
 
       if (!error && response.statusCode === 200) {
-        cbSuccess( processResponse(body) );
+        cbSuccess( new QueryData( url, processResponse(body), start ) );
       }
     });
   },
@@ -35,16 +60,23 @@ var ITUNES_URL = 'http://itunes.apple.com/',
   // usable format for easier consumption
   processResponse = function (data){
 
-    var storeReponse = JSON.parse(data),
+    var storeResponse = JSON.parse(data),
       apps = [];
 
-    if(storeReponse.resultCount === 0){
+    if(storeResponse.resultCount === 0){
       return;
     }
 
-    apps = storeReponse.results.map(function(respItem){
+    // make sure the items are actually apps and not developers info
+    storeResponse.results = storeResponse.results.filter(function(respItem){
+      return respItem && respItem.trackName !== undefined;
+    });
+
+    // build out a more usable object
+    apps = storeResponse.results.map(function(respItem){
 
       return {
+
         id: respItem.trackId,
         name: respItem.trackName,
         bundleId: respItem.bundleId,
@@ -55,7 +87,7 @@ var ITUNES_URL = 'http://itunes.apple.com/',
         developer: respItem.sellerName,
         developerUrl: respItem.sellerUrl,
         developerStoreUrl: respItem.artistViewUrl,
-        developerStoreId: respItem.artistId,
+        developerId: respItem.artistId,
         storeUrl: respItem.trackViewUrl,
 
         price: respItem.price,
@@ -93,7 +125,10 @@ var ITUNES_URL = 'http://itunes.apple.com/',
           tablet: respItem.ipadScreenshotUrls
         },
 
-        description: respItem.description
+        description: respItem.description,
+
+        languages: respItem.languageCodesISO2A
+
       };
 
     });
@@ -135,9 +170,10 @@ var ITUNES_URL = 'http://itunes.apple.com/',
       term = query;
     }
 
-    // given the term, see if it is an app id that is being searched for
+    // Given the term, see if it is an app id that is being searched for
     // so we can use the faster /lookup endpoint instead of the /search endpoint
-    useLookup = !isNaN(term) && term.length === 10;
+    // After aggregating hundreds of apps it seems the ids are always 9 digits
+    useLookup = !isNaN(term) && term.toString().length === 9;
 
     // Build the url
     return [
--- test/index.js
+++ test/index.js
@@ -1,25 +1,39 @@
 
+// TODO: don't be afraid, this will go under a more legit testing process
+
+
+
 console.log('Start AppStoreQuery Testing');
 
 
 var appStore = require('../src/AppStoreQuery.js');
 
 
-
-
 appStore.query({
-  term: 'angry birds',
-  limit: 20
-}, function(apps){
-
-  apps.forEach(function(app, index){
-    console.log(app.name);
+  term: 'b',
+  limit: 15
+}, function(queryData){
+  queryData.each(function(app, index){
+    console.log('apps starting with b: ', app.name);
   });
 
 });
 
 
+// get PopCap stuff
+/*
+appStore.query({
+  term: 284832145
+}, function(queryData){
 
+  console.log('request url', queryData.url);
+
+  var app = queryData.first();
+
+  console.log(app ? app.name + ' by ' + app.developer: 'App not found.');
+
+});
+*/
 
 
 /*appStore.query('2344589674');
